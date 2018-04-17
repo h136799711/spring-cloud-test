6567ca22a9717a77faabf2bfcc4ee7885eb05b6d
hebidu <hebiduhebi@126.com>
2018-04-17 18:54:31 +0800

    init config server

--- eureka-server/src/main/java/eureka/server/EurekaServerApplication.java
+++ eureka-server/src/main/java/eureka/server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.hebidu.springcloud.eurekaserver;
+package eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
