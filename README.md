32f6e24d63829aa96369cac2968bb6b7d645f46c
hebidu <hebiduhebi@126.com>
2018-04-19 13:46:05 +0800

    test spring.cloud.bus for update config

--- README.md
+++ README.md
@@ -1,9 +1,32 @@
 # spring-cloud-test
-spring cloud test
+spring cloud test 
+
+spring boot 2.0
+spring cloud  Finchley.M9
+jdk version 1.8
 
 
 #1. spring-cloud-config-server
 port: 8001
+##1.1 config-client
+port: 8081
+用于测试验证 Spring Config Server 是否成功配置
+##1.2 actuator 需要开启 bus-refresh 服务端口用于刷新配置
+
+management:
+  endpoints:
+    web:
+      exposure:
+        include:  ["health","info","shutdown", "refresh", "bus-refresh"]
 
 #2. spring-cloud-eureka-server
 port: 8002
+
+#3. spring-cloud-bus & spring-cloud-bus-amqp
+开启消息总线
+####目前选择了 rabbitmq 驱动，也可以选择其它   
+*localhost:5672    
+guest   
+136799711*
+
+
---
+++ config-client/src/main/java/com/hebidu/config/client/ConfigClientApplication.java
@@ -0,0 +1,26 @@
+package com.hebidu.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+@RefreshScope
+public class ConfigClientApplication {
+
+    @Value("${title}")
+    private String title;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @RequestMapping("/title")
+    public String title() {
+        return this.title;
+    }
+}
