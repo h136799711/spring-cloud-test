863fe0fb5e7c2816d54c1563751d5a0a35ab4fd4
hebidu <hebiduhebi@126.com>
2018-04-26 15:13:29 +0800

    【新增】

---
+++ microservice/bookstore/src/main/java/cn/hebidu/microservice/bookstore/BookstoreApplication.java
@@ -0,0 +1,14 @@
+package cn.hebidu.microservice.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class BookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
+}
