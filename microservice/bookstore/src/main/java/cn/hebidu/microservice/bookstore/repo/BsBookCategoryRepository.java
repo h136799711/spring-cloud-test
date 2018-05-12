f49228cbc15f083ede255196ce233c46f7cff489
hebidu <hebiduhebi@126.com>
2018-05-12 17:41:25 +0800

    【新增】配置

---
+++ microservice/bookstore/src/main/java/cn/hebidu/microservice/bookstore/repo/BsBookCategoryRepository.java
@@ -0,0 +1,8 @@
+package cn.hebidu.microservice.bookstore.repo;
+
+import cn.hebidu.microservice.bookstore.entities.BsBookCategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BsBookCategoryRepository extends JpaRepository<BsBookCategoryEntity, Integer> {
+
+}
--- microservice/bookstore/src/test/java/cn/hebidu/microservice/bookstore/BookstoreApplicationTests.java
+++ microservice/bookstore/src/test/java/cn/hebidu/microservice/bookstore/BookstoreApplicationTests.java
@@ -1,16 +1,28 @@
 package cn.hebidu.microservice.bookstore;
 
+import cn.hebidu.microservice.bookstore.entities.BsBookCategoryEntity;
+import cn.hebidu.microservice.bookstore.repo.BsBookCategoryRepository;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = BookstoreApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BookstoreApplicationTests {
+
+    @Autowired
+    BsBookCategoryRepository bookCategoryRepository;
 
     @Test
     public void contextLoads() {
+        Long id = 1L;
+        BsBookCategoryEntity bsBookCategoryEntity = bookCategoryRepository.getOne(id.intValue());
+        Assert.assertNotNull(bsBookCategoryEntity);
+        Long trueId = bsBookCategoryEntity.getId();
+        Assert.assertEquals(id, trueId);
     }
 
 }
